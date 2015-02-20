09d9477793ad25fac60d2cd5ed323eea659eea5d
Matthias Lemmer <matthias.lemmer@kks.uni-marburg.de>
2015-02-20 15:36:08 +0100

    - Initial Commit

---
+++ src/main/java/de/raptaml/ninja/hazelcast/embedded/CacheHazelcastImpl.java
@@ -0,0 +1,244 @@
+/*
+ * Copyright 2015 Matthias Lemmer.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.raptaml.ninja.hazelcast.embedded;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.GroupConfig;
+import com.hazelcast.config.InterfacesConfig;
+import com.hazelcast.config.NetworkConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import ninja.cache.Cache;
+import ninja.utils.NinjaProperties;
+import org.slf4j.Logger;
+
+/**
+ *
+ * @author lemmer
+ */
+@Singleton
+public class CacheHazelcastImpl implements Cache {
+    private final Logger logger;
+    private final NinjaProperties ninjaProperties;
+    
+    private final String bindAdress;
+    private final int bindPort;
+    private final String groupName;
+    private final String groupSecret;
+    
+    private final HazelcastInstance instance;
+    private final Config config;
+    private final NetworkConfig network;
+    private final GroupConfig group;
+    
+    private final IMap<String, Object> cache;
+    
+      
+    
+    
+    @Inject
+    public CacheHazelcastImpl(final Logger logger,
+                              final NinjaProperties ninjaProperties) throws Exception {
+        this.ninjaProperties = ninjaProperties;
+        
+        this.bindAdress = ninjaProperties.getOrDie("ninja.hazelcast.interface_ip");
+        this.bindPort = ninjaProperties.getIntegerOrDie("ninja.hazelcast.outbound_port");
+        this.groupName = ninjaProperties.getOrDie("ninja.hazelcast.groupname");
+        this.groupSecret = ninjaProperties.getOrDie("ninja.hazelcast.groupsecret");
+        
+        this.logger = logger;
+                
+        this.config = new Config();
+        network = new NetworkConfig().setInterfaces(
+                                                    new InterfacesConfig().addInterface(bindAdress))
+                                     .setPort(bindPort);
+        group = new GroupConfig(groupName, groupSecret);
+        
+        config.setNetworkConfig(network);
+        config.setGroupConfig(group);
+        
+        this.instance = Hazelcast.newHazelcastInstance(config);
+        
+        cache = instance.getMap("cache");
+        
+                
+    }
+    
+    
+
+    @Override
+    public void add(String key, Object value, int expiration) {
+        cache.putIfAbsent(key, value, expiration, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public boolean safeAdd(String key, Object value, int expiration) {
+        //API will return
+        try {
+            if (!cache.tryLock(key, 2, TimeUnit.SECONDS)) {
+                return false;
+            }
+            return cache.putIfAbsent(key, value, expiration, TimeUnit.SECONDS) == null ;
+        } catch (InterruptedException ex) {
+            return false;
+        } finally {
+            cache.unlock(key);
+        }
+        
+        
+    }
+
+    @Override
+    public void set(String key, Object value, int expiration) {
+        cache.set(key, value, expiration, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public boolean safeSet(String key, Object value, int expiration) {
+        
+        try {
+            if (!cache.tryLock(key, 2, TimeUnit.SECONDS)) {
+            return false;
+            }
+            try {
+                cache.set(key, value, expiration, TimeUnit.SECONDS);
+                return cache.get(key).equals(value);
+            } finally {
+                cache.unlock(key);
+            }
+        } catch (Exception e) {
+            return false;
+            
+        }
+        //cache.set(key, value, expiration, TimeUnit.SECONDS);
+        //boolean DEBUG = (cache.containsKey(key) && cache.get(key).equals(value));
+        //return (cache.containsKey(key) && cache.get(key).equals(value));
+        
+    }
+
+    @Override
+    public void replace(String key, Object value, int expiration) {
+        if (cache.containsKey(key)) {
+            cache.set(key, value,expiration, TimeUnit.SECONDS);
+        }
+    }
+     
+    @Override
+    public boolean safeReplace(String key, Object value, int expiration) {
+        try {
+            if (!cache.tryLock(key, 2, TimeUnit.SECONDS)) {
+            return false;
+            }
+            try {
+                Object oldValue = cache.get(key);
+                if (cache.containsKey(key) && cache.get(key).equals(oldValue)) {
+                    cache.set(key, value, expiration, TimeUnit.SECONDS);
+                    return true;
+                }
+                return false;
+            } finally {
+                cache.unlock(key);
+            }
+        }catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public Object get(String key) {
+        return cache.get(key);
+    }
+
+    @Override
+    public Map<String, Object> get(String[] keys) {
+        return cache.getAll(new LinkedHashSet<>(Arrays.asList(keys)));
+    }
+
+    //DEADLOCK WAITING POSSIBLE !!
+    @Override
+    public long incr(String key, int by) {
+        if (!cache.tryLock(key)) {
+            throw new IllegalStateException("The specified key is locked by another thread");
+        }
+        try {
+            //this will throw if cached Object is not a Long
+            cache.get(key).getClass().asSubclass(Long.class);
+            long num =(long) cache.get(key);
+            num = num + by;
+            return (long)cache.put(key, (Long)num);
+        }catch (ClassCastException e){
+            throw new ClassCastException("Can only increment subtype of Long");
+        //prevent dead locks at any cost    
+        } finally {
+            cache.unlock(key);
+        }
+ 
+    }
+    //DEADLOCK WAITING POSSIBLE !!
+    @Override
+    public long decr(String key, int by) {
+        if (!cache.tryLock(key)) {
+            throw new IllegalStateException("The specified key is locked by another thread");
+        }
+        try {
+            //this will throw if cached Object is not a Long
+            cache.get(key).getClass().asSubclass(Long.class);
+            long num =(long) cache.get(key);
+            num = num - by;
+            return (long)cache.put(key, (Long)num);
+        }catch (ClassCastException e){
+            throw new ClassCastException("Can only decrement subtype of Long");
+        //prevent dead locks at any cost    
+        } finally {
+            cache.unlock(key);
+        }
+    }
+
+    @Override
+    public void clear() {
+        cache.clear();
+    }
+
+    @Override
+    public void delete(String key) {
+        cache.delete(key);
+    }
+
+    @Override
+    public boolean safeDelete(String key) {
+        return cache.tryRemove(key, 2, TimeUnit.SECONDS);
+    }
+    
+     //Methods for testing purposes. Not exposed via Interface
+    public void destroyCacheService() {
+        this.instance.shutdown();
+    }
+    public void lock(String key, long seconds) {
+        cache.lock(key,seconds,TimeUnit.SECONDS);
+    }
+    public void unlock(String key) {
+        cache.unlock(key);
+    }
+    
+    
+}
---
+++ src/test/java/de/raptaml/ninja/hazelcast/embedded/CacheHazelcastImplTest.java
@@ -0,0 +1,312 @@
+/*
+ * Copyright 2015 Matthias Lemmer.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.raptaml.ninja.hazelcast.embedded;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import java.util.LinkedHashMap;
+import ninja.Configuration;
+import ninja.NinjaTest;
+import ninja.lifecycle.LifecycleSupport;
+import ninja.utils.NinjaConstant;
+import ninja.utils.NinjaMode;
+import ninja.utils.NinjaPropertiesImpl;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.slf4j.Logger;
+
+
+
+/**
+ *
+ * @author lemmer
+ */
+public class CacheHazelcastImplTest extends NinjaTest {
+    
+    NinjaPropertiesImpl ninjaProperties = new NinjaPropertiesImpl(NinjaMode.test);
+    Injector injector = Guice.createInjector(new Configuration(ninjaProperties), LifecycleSupport.getModule());
+    Logger logger = injector.getInstance(Logger.class);
+    CacheHazelcastImpl cache;
+    static boolean run = false;
+      
+    @Before
+    public void init() throws Exception{
+        
+        
+            ninjaProperties.setProperty(NinjaConstant.CACHE_IMPLEMENTATION, "ninja.cache.CacheHazelcastImpl");
+            ninjaProperties.setProperty("ninja.hazelcast.interface_ip","127.0.0.1");
+            ninjaProperties.setProperty("ninja.hazelcast.outbound_port","5701");
+            ninjaProperties.setProperty("ninja.hazelcast.groupname", "group1");
+            ninjaProperties.setProperty("ninja.hazelcast.groupsecret", "8df77g7fjkljr9ursamd034j");
+            
+            cache = new CacheHazelcastImpl(logger, ninjaProperties);
+        
+        
+    }
+    
+    @After
+    public void terminate() {
+      cache.destroyCacheService();
+    }
+   
+    @Test
+    public void testAdd() {
+        cache.add("1", "String", 0);
+        Assert.assertEquals("String", cache.get("1"));
+        
+        cache.add("2", 22, 0);
+        Assert.assertEquals(22, cache.get("2"));
+        
+        cache.add("3", 3.3, 0);
+        Assert.assertEquals(3.3, cache.get("3"));
+        
+        cache.add("4", true, 0);
+        Assert.assertEquals(true, cache.get("4"));
+        
+    }
+    
+    @Test
+    public void testSafeAdd() {
+        Assert.assertTrue(cache.safeAdd("61", "String", 0));
+        Assert.assertFalse(cache.safeAdd("61", "String2", 0));
+        Assert.assertEquals("String", cache.get("61"));
+        
+    }
+    
+    @Test
+    public void testSet() {
+        cache.set("1", "String", 0);
+        Assert.assertEquals("String", cache.get("1")); 
+    }
+    
+    @Test
+    public void testSafeSet() throws Exception {
+        Assert.assertTrue(cache.safeSet("1", "String", 0));
+        
+        final CacheHazelcastImpl cache2 = new CacheHazelcastImpl(logger, ninjaProperties);
+
+        //Spawn new thread for simulating foreign lock-holder
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                cache2.lock("1", 100);
+            }
+        };
+        t.start();
+
+        //wait for other thread to finish so lock is taken
+        synchronized(t) {
+            t.wait();
+            //should fail while locked by t
+            Assert.assertFalse(cache.safeSet("1", "String2", 0));
+        }        
+        cache2.destroyCacheService();
+        
+        //should now work as cache2 is destroyed and therfore the lock is gone...
+        Assert.assertTrue(cache.safeSet("1", "String2", 0));
+        
+    }
+    
+    @Test
+    public void testReplace() {
+        cache.set("1", "Value", 0);
+        cache.replace("1","REPLACED",0);
+        Assert.assertEquals("REPLACED", cache.get("1"));
+        
+    }
+    
+    @Test
+    public void testSafeReplace() throws Exception {
+        cache.set("1", "Value", 0);
+        Assert.assertTrue(cache.safeReplace("1","REPLACED",0));
+        Assert.assertFalse(cache.safeReplace("2","REPLACED",0));
+                
+        final CacheHazelcastImpl cache2 = new CacheHazelcastImpl(logger, ninjaProperties);
+        //Spawn new thread for simulating foreign lock-holder
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                cache2.lock("1", 100);
+            }
+        };
+        t.start();
+        
+        synchronized(t) {
+            t.wait();
+            //should fail while locked by t
+            Assert.assertFalse(cache.safeReplace("1", "String2", 0));
+        }        
+        cache2.destroyCacheService();
+        Assert.assertTrue(cache.safeReplace("1","REPLACED",0));
+    }
+    
+    @Test
+    public void testGet() {
+        cache.set("1", "Test", 0);
+        Assert.assertNotEquals(cache.get("1"), null);
+        Assert.assertEquals(cache.get("2"), null);
+    }
+    
+    @Test
+    public void testGetMultiple() {
+        String[] sArr = {"1","2","3"};
+        LinkedHashMap<String,Object> hMap = new LinkedHashMap<>();
+        
+        for (String sArr1 : sArr) {
+            cache.set(sArr1, "String"+sArr1, 0);
+            hMap.put(sArr1,"String"+sArr1);
+        }
+        
+        Assert.assertEquals(cache.get(sArr),hMap);
+                
+    }
+    
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    @Test
+    public void testIncr() throws Exception {
+        Long counter = 0L;
+        Integer intCounter = 0;
+        
+        cache.set("counter",counter,0);
+        cache.incr("counter", 1);
+        
+        Assert.assertEquals(1L, cache.get("counter"));
+        
+        cache.set("counter",counter,0);
+        cache.incr("counter", -10);
+        
+        Assert.assertEquals(-10L, cache.get("counter"));
+        
+        cache.set("counter",intCounter,0);
+        exception.expect(ClassCastException.class);
+        exception.expectMessage("Can only increment subtype of Long");
+        cache.incr("counter", 1);
+        
+        final CacheHazelcastImpl cache2 = new CacheHazelcastImpl(logger, ninjaProperties);
+        cache.set("counter", counter, 0);
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                cache2.lock("counter", 100);
+            }
+        };
+        t.start();
+
+        //wait for other thread to finish so lock is taken
+        synchronized(t) {
+            t.wait();
+            //should fail while locked by t
+            
+            exception.expect(IllegalStateException.class);
+            exception.expectMessage("The specified key is locked by another thread");
+            cache.incr("counter", 1);
+        }        
+        cache2.destroyCacheService();
+        
+    }
+    
+    @Test
+    public void testDecr() throws Exception {
+        Long counter = 2L;
+        Integer intCounter = 2;
+        
+        cache.set("counter",counter,0);
+        cache.decr("counter", 1);
+        
+        Assert.assertEquals(1L, cache.get("counter"));
+        
+        cache.set("counter",counter,0);
+        cache.decr("counter", 10);
+        
+        Assert.assertEquals(-8L, cache.get("counter"));
+        
+        cache.set("counter",intCounter,0);
+        exception.expect(ClassCastException.class);
+        exception.expectMessage("Can only decrement subtype of Long");
+        cache.decr("counter", 1);
+        
+        
+        final CacheHazelcastImpl cache2 = new CacheHazelcastImpl(logger, ninjaProperties);
+        cache.set("counter", counter, 0);
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                cache2.lock("counter", 100);
+            }
+        };
+        t.start();
+
+        //wait for other thread to finish so lock is taken
+        synchronized(t) {
+            t.wait();
+            //should fail while locked by t
+            
+            exception.expect(IllegalStateException.class);
+            exception.expectMessage("The specified key is locked by another thread");
+            cache.decr("counter", 1);
+        }        
+        cache2.destroyCacheService();
+        
+    }
+    
+    @Test
+    public void testClear() {
+        cache.set("1", "String", 0);
+        cache.clear();
+        Assert.assertNull(cache.get("1"));
+        
+    }
+    
+    @Test
+    public void testDelete() {
+        cache.set("1", "String", 0);
+        cache.delete("1");
+        Assert.assertNull(cache.get("1"));
+    }
+    
+    @Test
+    public void testSafeDelete() throws Exception {
+        cache.set("1", "String", 0);
+        Assert.assertTrue(cache.safeDelete("1"));
+        
+        final CacheHazelcastImpl cache2 = new CacheHazelcastImpl(logger, ninjaProperties);
+        cache.set("1", "String", 0);
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                cache2.lock("1", 100);
+            }
+        };
+        t.start();
+
+        //wait for other thread to finish so lock is taken
+        synchronized(t) {
+            t.wait();
+            //should fail while locked by t
+            Assert.assertFalse(cache.safeDelete("1"));
+        }        
+        cache2.destroyCacheService();
+        
+        
+        
+    }
+    
+}
